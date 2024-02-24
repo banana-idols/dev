bd33ec64b163e004f1f9ebd52cd1063d7ff00b03
banana-idols <ceklon@gmail.com>
2024-02-24 21:42:45 +0200

    Dark theme and bootstrap

--- gUIs_in_python_with tkinter/main.py
+++ gUIs_in_python_with tkinter/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+# from tkinter import ttk
+import ttkbootstrap as ttk
 
 
 def convert():
@@ -9,7 +10,7 @@ def convert():
 
 
 # window
-window = tk.Tk()
+window = ttk.Window(themename="darkly")
 window.title('Demo')
 window.geometry('300x150')
 
