500638b260d27ca317da8f3a92a09aa638ea4109
banana-idols <ceklon@gmail.com>
2024-02-24 21:28:39 +0200

    Convertor Base Functionality

--- gUIs_in_python_with tkinter/main.py
+++ gUIs_in_python_with tkinter/main.py
@@ -1,26 +1,39 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def convert():
+    mile_input = entry_int.get()
+    km_output = mile_input * 1.61
+    output_string.set(km_output)
+
+
 # window
 window = tk.Tk()
 window.title('Demo')
 window.geometry('300x150')
 
 # title
-title_label = ttk.Label(master = window, text = "Miels to Kilometars", font = "Calibri 24 bold")
+title_label = ttk.Label(master=window, text="Miles to Kilometres", font="Calibri 24 bold")
 title_label.pack()
 
 # input field
-input_frame = ttk.Frame(master = window)
-entry = ttk.Entry(master = input_frame)
-button = ttk.Button(master = input_frame, text = "Convert")
-entry.pack(side = 'left', padx = 10)
-button.pack(side = 'left')
-input_frame.pack(pady = 10)
+input_frame = ttk.Frame(master=window)
+entry_int = tk.IntVar()
+entry = ttk.Entry(master=input_frame, textvariable=entry_int)
+button = ttk.Button(master=input_frame, text="Convert", command=convert)
+entry.pack(side='left', padx=10)
+button.pack(side='left')
+input_frame.pack(pady=10)
 
 # output
-output_label = ttk.Label(master = window, text = "Output")
-output_label.pack()
+output_string = tk.StringVar()
+output_label = ttk.Label(
+    master=window,
+    text="Output",
+    font="Calibri 24",
+    textvariable=output_string)
+output_label.pack(pady=5)
 
 # run
 window.mainloop()
