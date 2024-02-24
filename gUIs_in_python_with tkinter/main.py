e7ecbc7214663a55a4e8779a272fbfd8b75534ec
banana-idols <ceklon@gmail.com>
2024-02-24 20:53:15 +0200

    Output UI ready

--- gUIs_in_python_with tkinter/main.py
+++ gUIs_in_python_with tkinter/main.py
@@ -6,5 +6,21 @@ window = tk.Tk()
 window.title('Demo')
 window.geometry('300x150')
 
+# title
+title_label = ttk.Label(master = window, text = "Miels to Kilometars", font = "Calibri 24 bold")
+title_label.pack()
+
+# input field
+input_frame = ttk.Frame(master = window)
+entry = ttk.Entry(master = input_frame)
+button = ttk.Button(master = input_frame, text = "Convert")
+entry.pack(side = 'left', padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+# output
+output_label = ttk.Label(master = window, text = "Output")
+output_label.pack()
+
 # run
 window.mainloop()
