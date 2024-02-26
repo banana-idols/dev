79396aa3ddef9347968868877e42ca8c427957e7
banana-idols <ceklon@gmail.com>
2024-02-26 17:43:26 +0200

    bootstrap styling

--- python/tkinter/main-tkinter.py
+++ python/tkinter/main-tkinter.py
@@ -1,23 +1,24 @@
 import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
 
 # window
-window = tk.Tk()
+window = ttk.Window(themename="darkly")
 window.title ("Banana-Idols")
 window.geometry('800x500')
 
 # widgets
-title_label = ttk.Label(master = window, 
-                        text = "Miles to Kilometars", 
-                        font="Calibri 24 bold")
+title_label = ttk.Label(
+    master = window, 
+    text = "Miles to Kilometars", 
+    font="Calibri 24 bold")
 title_label.pack()
 
 input_frame = ttk.Frame(master = window)
-input_frame.pack()
 entry = ttk.Entry(master = input_frame)
 entry.pack(side = "left")
 button = ttk.Button(master = input_frame, text = "Convert")
 button.pack(side = "left")
+input_frame.pack()
 
 # run
 window.mainloop()
